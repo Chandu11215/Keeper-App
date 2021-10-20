1a981fba90f511547296488a265cfaa84f59325f
kalal Chandrashekar <kalalchandrashekar9963@gmail.com>
2021-10-20 14:09:17 +0530

    keeper app changes

---
+++ build/precache-manifest.1b854368e8dcdac04fc0e897125689ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc44f2197b250d09a36d48d071965ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2ca5322fa0b9c46086",
+    "url": "/static/js/2.f86ea32c.chunk.js"
+  },
+  {
+    "revision": "51ef30a137812068cafc",
+    "url": "/static/js/main.dc930d64.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
